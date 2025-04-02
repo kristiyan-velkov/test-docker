4e55729487092f3b8f93b3ff001671db2e2acb27
kristiyan.velkov <kristiyan.velkov@ffw.com>
2025-04-02 21:44:22 +0300

    [feat] added vite config and Dockerfile dev

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/",
   plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+  },
   test: {
     environment: "jsdom",
     setupFiles: "./src/setupTests.ts",
